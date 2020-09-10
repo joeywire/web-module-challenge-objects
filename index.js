cf5b51459e0b6b0b0fbe8876c4db94034ca22a29
Joe L <joseph.lasata@gmail.com>
2020-09-10 10:51:40 -0500

    Completed through task 6

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ burger.discount = function(discountType) {
 // console.log(burger.discount('Student')); 
 ///////////////Reviews (MVP)///////////////////
 
-const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and wonderful vegan options!"},
+const reviews = [
+    {name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and wonderful vegan options!"},
     {name: "Jack", rating: 3, feedback:"A little too hipster for my taste, but the burger was decent, if overpriced"},
     {name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
     {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
@@ -56,12 +57,22 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 ]
 
 /* Task 3: Console.log just Julius' feedback */
-
+console.log(reviews[5].feedback); 
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
+reviews.push({
+  name: 'Joe',
+  rating: 3, 
+  feedback: "Great atmosphere and company but the menu is very limited!",
+}); 
 
+// console.log(reviews[reviews.length -1]); 
 
-/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
+/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays" */
+
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays"; 
+
+// console.log(reviews[7]); 
 
 /*  Task 6: Write a function to return a review based on the index of the review in the array.
 
@@ -76,8 +87,11 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
 function getReviewByIndex(reviews, index) {
-    /* code here */
-  }
+    return reviews[index].feedback; 
+  }; 
+
+
+// console.log(getReviewByIndex(reviews, 0)); 
   
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
